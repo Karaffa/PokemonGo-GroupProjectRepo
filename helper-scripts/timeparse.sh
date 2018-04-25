ad4e3953d2c6c3cfa5e120f480a353790f6f3588
Pierre Karaffa <35781401+Karaffa@users.noreply.github.com>
2018-04-25 10:37:45 -0400

    Options to use -t to get to time parser and more

--- helper-scripts/timeparse.sh
+++ helper-scripts/timeparse.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
 #takes a file and searches for the time
-if [ $1 ]; then
-	cat $1 | sed -n -e 's/^.*Time: //p'
+if [ $1 == "-t" ]; then
+	if [ $2 != "-p" ]; then
+		cat $2 | sed -n -e 's/^.*Time: //p'
+	else
+		cat $3 | sed -n -e 's/^.*Time: //p' | awk '{print substr($0,2,2)}' | sort | uniq -c | sort -bnr
+	fi
+	
 else
-	echo Expected a file to parse...
+	cat $1 | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr > wordoccur.txt
 fi
+
+#cat times.txt | awk '{print substr($0,2,2)}'
