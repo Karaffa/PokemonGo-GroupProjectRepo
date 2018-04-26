406038384538f436d565c814bb7e1ae0d1b5d6f5
josuef6 <josuef@g.clemson.edu>
2018-04-26 01:29:02 -0400

    Final Word Cloud (hopefully)

--- helper-scripts/wordcloud.R
+++ helper-scripts/wordcloud.R
@@ -14,7 +14,7 @@ library(readr)
 
 #Creates Word Cloud AFTER removing "Tweet", "Time", and "User"
 #Destination of .csv for Josue
-wordCloudTweets <- read_csv("~/Desktop/PokemonGo-GroupProjectRepo/data/forwordcloud.csv")
+wordCloudTweets <- read_csv("~/Desktop/PokemonGo-GroupProjectRepo/data/userparseddump.csv")
 
 #Destination of .csv for Jessica
 #wordCloudTweets <- read_csv("~/Desktop/PokemonGo-GroupProjectRepo/data/forwordcloud.csv")
@@ -28,14 +28,16 @@ wordCloudCorpus <- tm_map(wordCloudCorpus, removePunctuation)
 wordCloudCorpus <- tm_map(wordCloudCorpus, PlainTextDocument)
 wordCloudCorpus <- tm_map(wordCloudCorpus, removeWords, stopwords('english'))
 wordCloudCorpus <- tm_map(wordCloudCorpus, removeWords, stopwords('spanish'))
-wordCloudCorpus <- tm_map(wordCloudCorpus, removeWords, c("pokemongo", "httpst…", 
-     "retweet", "httpstcokbititppf", "pokemon", "ポケモンgo","ポケモンの巣",
-     "httpstcokbititppf", "pokémon", "twi...", "group...", "will","...",
-     "httpst...", "pikachu...", "know", "look", "just", "like", "join", "aquí",
-     "u00001f680", "u0001f534", "u0001f31f", "u0001f525", "ポケモン",
-     "出現報告", "ポケgo", "httpst…", "pikachu…", "yeah", "ポケモンgo",
-     "u0001f60", "u0001f680", "10000", "group", "still", "httpstcokbititppf",
-     "無料で誰でも気軽に出現情報を共有できる♪匿名okなオープンチャット◎"))
+wordCloudCorpus <- tm_map(wordCloudCorpus, removeWords, c("user", "tweet", "time",
+    "pokemongo", "httpst…", "...", "httpstcokbititppf", "pikachu...",
+    "retweet", "httpstcokbititppf", "pokemon", "ポケモンgo","ポケモンの巣",
+    "httpstcokbititppf", "pokémon", "twi...", "group...", "will","...",
+    "httpst...", "know", "look", "just", "like", "join", "aquí",
+    "u00001f680", "u0001f534", "u0001f31f", "u0001f525", "ポケモン",
+    "出現報告", "ポケgo", "httpst…", "pikachu…", "yeah", "ポケモンgo",
+    "u0001f60", "u0001f680", "10000", "group", "still", "httpstcokbititppf",
+    "無料で誰でも気軽に出現情報を共有できる♪匿名okなオープンチャット◎",
+    "dont", "want", "thank", "want", "play", "telegram", "video"))
 wordCloudCorpus <- tm_map(wordCloudCorpus, stemDocument)
 wordcloud(wordCloudCorpus, max.words = 50, random.order = FALSE, random.color = TRUE, 
           colors = rev(colorRampPalette(brewer.pal(8,"Paired"))(32)), scale=c(5, .5))
